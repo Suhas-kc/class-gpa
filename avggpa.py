bfd41813a20bbbd91fdcfb7207ff7d38c64874c5
Susamak <suhasksl@gmail.com>
2016-04-14 19:42:12 +0530

    Added files via upload

---
+++ avggpa.py
@@ -0,0 +1,30 @@
+import urllib
+from BeautifulSoup import BeautifulSoup
+import time
+
+usnlst = []
+for i in range(66,127):
+    if i == 110: continue                               #Makes a list of usn's from 066 to 126
+    if i < 100:
+        usnlst.append('1DT15CS0'+str(i))
+    else:
+        usnlst.append('1DT15CS'+str(i))
+        
+
+
+sumgpa = 0.0
+
+for usn in usnlst:
+    url = 'http://result.vtu.ac.in/cbcs_results2016.aspx?usn='
+    url = url + usn  #Gives the result url for that usn
+    urlh = urllib.urlopen(url)  
+    data = urlh.read()      #Reads the html file of the results page
+    soup = BeautifulSoup(data)  #Creates a BeautifulSoup object of the html file
+    gpa = float(soup.find(id = "lblSGPA").b.string)
+    sumgpa += gpa
+    print usn + '  ' + str(gpa)
+    time.sleep(1)
+    
+avggpa = sumgpa/float(len(usnlst))
+print "Average gpa is: %.2f" %(avggpa)      #Prints average gpa of class
+     
