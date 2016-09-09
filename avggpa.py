480f5f959a8a806ecefd564106965a1045efef0a
Susamak <suhasksl@gmail.com>
2016-09-09 20:36:48 +0530

    Edited file for second sem results

--- avggpa.py
+++ avggpa.py
@@ -4,27 +4,52 @@ import time
 
 usnlst = []
 for i in range(66,127):
-    if i == 110: continue                               #Makes a list of usn's from 066 to 126
+    if i == 110 or i == 106: continue                                               #Makes a list of usn's from 066 to 126
     if i < 100:
-        usnlst.append('1XX15YY0'+str(i))                #Made some changes due to privacy issues
+        usnlst.append('1DT15CS0'+str(i))
     else:
-        usnlst.append('1XX15YY'+str(i))
+        usnlst.append('1DT15CS'+str(i))
         
 
-
+missedlist = []
 sumgpa = 0.0
-
+highest = 0.0
+highlst = []
 for usn in usnlst:
     url = 'http://result.vtu.ac.in/cbcs_results2016.aspx?usn='
-    url = url + usn                                     #Gives the result url for that usn
+    url = url + usn + '&sem=2'                                                    #Gives the result url for that usn
     urlh = urllib.urlopen(url)  
-    data = urlh.read()                                  #Reads the html file of the results page
-    soup = BeautifulSoup(data)                          #Creates a BeautifulSoup object of the html file
-    gpa = float(soup.find(id = "lblSGPA").b.string)     #Grabs the gpa from the html file
+    data = urlh.read()                                                  #Reads the html file of the results page
+    soup = BeautifulSoup(data)
+    try:                                          #Creates a BeautifulSoup object of the html file
+        gpa = float(soup.find(id = "lblSGPA").b.string)
+    except:
+        missedlist.append(url)
+        continue
     sumgpa += gpa
+    if gpa >= highest:
+        highest = gpa
+        highlst.append((usn,gpa))
     print usn + '  ' + str(gpa)
-    time.sleep(1)
-    
-avggpa = sumgpa/float(len(usnlst))
-print "Average gpa is: %.2f" %(avggpa)                  #Prints average gpa of class
+ 
+n = len(missedlist)
+while(len(missedlist)>0):
+   url = missedlist[0]
+   urlh = urllib.urlopen(url)  
+   data = urlh.read()                                                  #Reads the html file of the results page
+   soup = BeautifulSoup(data)
+   try:                                          #Creates a BeautifulSoup object of the html file
+       gpa = float(soup.find(id = "lblSGPA").b.string)
+   except:
+        continue
+   sumgpa += gpa
+   missedlist.pop(0)
+   if gpa >= highest:
+        highest = gpa
+        highlst.append((url[50:-5],gpa))
+   print url[50:-5] + '  ' + str(gpa)
+ 
+avggpa = sumgpa/len(usnlst)
+print "Average gpa is: %.2f" %(avggpa)                                #Prints average gpa of class
      
+print "Highest gpa is : " , highlst
